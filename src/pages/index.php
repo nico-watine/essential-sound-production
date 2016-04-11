b94418e5ff5193c5fb1b8fd211d953f898a506b7
Nico Watine <nico@studioncreations.com>
2016-04-11 13:38:51 -0500

    Progress on index nav

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,8 +1,26 @@
 ---
 body-id: index
 ---
-<section class="landing">
-	<h1>SOUND | LIGHTING | DJ ENTERTAINMENT</h1>
+<header>
+	<div class="row">
+		<div class="medium-5 columns text-right">
+			<a href="#" class="first">SERVICES</a>
+			<a href="#">GALLERY</a>
+		</div>
+		<div class="medium-2 columns text-center">
+			<img srcset="/img/assets/logo/esp-logo@2x.png 2x" src="/img/assets/logo/esp-logo.png" alt="" class="logo">
+		</div>
+		<div class="medium-5 columns">
+			<a href="#" class="first">ABOUT</a>
+			<a href="#">CONTACT</a>
+		</div>
+	</div>
+</header>
+<section class="landing text-center">
+	<div class="prose-wrapper">
+		<h1>ESSENTIAL SOUND PRODUCTION</h1>
+		<h2>SOUND | LIGHTING | DJ ENTERTAINMENT</h2>
+	</div>
 </section>
 <section class="services">
 	<div class="row">
