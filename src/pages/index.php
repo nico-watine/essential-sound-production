953fab7e3191ac2cf29464e966938e811fe095ce
Nico Watine <nico@studioncreations.com>
2016-04-12 11:39:26 -0500

    index#about DONE
    Pretty sure @ all breakpoints

--- src/pages/index.php
+++ src/pages/index.php
@@ -15,5 +15,20 @@ body-id: index
 	</div>
 </section>
 <section class="about">
-	
+	<div class="row header">
+		<div class="small-12 columns">
+			<h2 class="title"><span>ABOUT ESP</span></h2>
+			<br>
+			<br>
+		</div>
+	</div>
+	<div class="row">
+		<div class="small-12 columns">
+			<div class="prose-wrapper">
+				<p>Since 2008, ESP has set the standards to a new level each year. Essential Sound Production aims for class, customer satisfaction, and of course to create a memorable experience.</p>
+				<br>
+				<p>Essential Sound Production's primary goal is to connect with our clients through music. Our passion and expertise in music genres guarantee an atmosphere that truly creates an unforgettable memory. </p>
+			</div>
+		</div>
+	</div>
 </section>
