b7f780f39e3a9178bf8eda208f4e998bbb51b84a
Nico Watine <nico@studioncreations.com>
2016-04-12 10:43:05 -0500

    Progress on top-nav & index#about

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,29 +1,19 @@
 ---
 body-id: index
 ---
-<header>
-	<div class="row">
-		<div class="medium-5 columns text-right">
-			<a href="#" class="first">SERVICES</a>
-			<a href="#">GALLERY</a>
-		</div>
-		<div class="medium-2 columns text-center">
-			<img srcset="/img/assets/logo/esp-logo@2x.png 2x" src="/img/assets/logo/esp-logo.png" alt="" class="logo">
-		</div>
-		<div class="medium-5 columns">
-			<a href="#" class="first">ABOUT</a>
-			<a href="#">CONTACT</a>
-		</div>
-	</div>
-</header>
+{{> nav-1}}
 <section class="landing text-center">
 	<div class="prose-wrapper">
-		<h1>ESSENTIAL SOUND PRODUCTION</h1>
-		<h2>SOUND | LIGHTING | DJ ENTERTAINMENT</h2>
+		<h1 class="title">ESSENTIAL SOUND PRODUCTION</h1>
+		<h2 class="title">SOUND | LIGHTING | DJ ENTERTAINMENT</h2>
 	</div>
 </section>
 <section class="services">
-	<div class="row">
-		<div class="small-12 columns" style="background: red; height: 300px;">&nbsp;</div>
+	<div class="row text-center">
+		<h2 class="title">OUR SERVICES</h2>
+		<h3 class="body-font expanded">We offer a variety of event service packages to best fit your needs.</h3>
 	</div>
+</section>
+<section class="about">
+	
 </section>
