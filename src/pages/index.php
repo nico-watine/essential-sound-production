a48578340a212f7b3c88e60bcb33bfb07c6cbf4c
Nico Watine <nico@studioncreations.com>
2016-04-28 11:02:44 -0500

    Progress on section.gallery -> switching to section.services with fresh idea
    See .sketch file with info on services ideas

--- src/pages/index.php
+++ src/pages/index.php
@@ -80,6 +80,44 @@ body-id: index
 		</div>
 	</div>
 </section>
+<section class="photo-gallery">
+	<div class="row header text-center">
+		<h2 class="title"><span>PHOTO GALLERY</span></h2>
+	</div>
+	<div class="row">
+		<!-- <div class="small-12 columns"> -->
+			<div class="orbit" role="region" aria-label="Essential Sound Production Photo Gallery" data-orbit>
+				<ul class="orbit-container">
+					<button class="orbit-previous"><span class="show-for-sr">Previous Slide</span> &#9664;&#xFE0E;</button>
+					<button class="orbit-next"><span class="show-for-sr">Next Slide</span> &#9654;&#xFE0E;</button>
+					<li class="orbit-slide is-active">
+						<img class="orbit-image" src="/img/index/gallery/slides/1.jpg" alt="">
+						<!-- <figcaption class="orbit-caption">Space, the final frontier.</figcaption> -->
+					</li>
+					<li class="orbit-slide">
+						<img class="orbit-image" src="/img/index/gallery/slides/2.jpg" alt="">
+						<!-- <figcaption class="orbit-caption">Space, the final frontier.</figcaption> -->
+					</li>
+					<li class="orbit-slide">
+						<img class="orbit-image" src="/img/index/gallery/slides/3.jpg" alt="">
+						<!-- <figcaption class="orbit-caption">Space, the final frontier.</figcaption> -->
+					</li>
+					<li class="orbit-slide">
+						<img class="orbit-image" src="/img/index/gallery/slides/4.jpg" alt="">
+						<!-- <figcaption class="orbit-caption">Space, the final frontier.</figcaption> -->
+					</li>
+				</ul>
+				<nav class="orbit-bullets">
+					<button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+					<button data-slide="1"></button>
+					<!-- <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button> -->
+					<button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+					<button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
+				</nav>
+			</div>
+		<!-- </div> -->
+	</div>
+</section>
 <section class="about">
 	<div class="row header">
 		<div class="small-12 columns">
