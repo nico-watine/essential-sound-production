ac777dc1e02a42c4aae0e51e723010f5d7ef2d9a
Nico Watine <nico@studioncreations.com>
2016-04-28 12:27:44 -0500

    ULTRA package details set
    Now play with 6/6 premium/deluxe

--- src/pages/services.php
+++ src/pages/services.php
@@ -39,5 +39,86 @@ body-id: services
 				<p>$1,000</p>
 			</div>
 		</div>
+		<div class="small-12 columns">
+			<div class="package-plan" id="deluxe">
+				<h3 class="title">DELUXE</h3>
+				<ul class="accordion" data-accordion data-allow-all-closed="true">
+					<li>6 Hours of Music<div data-tab-content></div></li>
+					<li>Premium QSC Sound Speakers<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Speaker Details</a>
+						<div class="accordion-content" data-tab-content>
+							4 - K12 Speakers <br>2 - KSub Speaker
+						</div>
+					</li>
+					<li>4 Aluminum Truss &amp; 4 Moving Light Heads<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>20 Up Lighting<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>4 LED Dance Lights<div data-tab-content></div></li>
+					<li>Custom Gobo Stencil<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>Wireless Mic &amp; DJ<div data-tab-content></div></li>
+				</ul>
+				<p>$1,200</p>
+			</div>
+		</div>
+		<div class="small-12 columns">
+			<div class="package-plan" id="ultra">
+				<h3 class="title">ULTRA</h3>
+				<ul class="accordion" data-accordion data-allow-all-closed="true">
+					<li>6 Hours of Music<div data-tab-content></div></li>
+					<li>Premium QSC Sound Speakers<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Speaker Details</a>
+						<div class="accordion-content" data-tab-content>
+							<span style="font-size: 16px;">- Complete Setup According To Venue Size -</span><br>
+							2 - K153/152 Speakers <br>
+							4 - K12 Speakers <br>
+							4 - JBL PRX 18" Sub Speakers
+						</div>
+					</li>
+					<li>4 Aluminum Truss &amp; 4 7R SkyBeam Moving Heads<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>30 Up Lighting<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>4 LED Dance Lights<div data-tab-content></div></li>
+					<li>Custom Gobo Stencil<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>2 - Wireless Mic &amp; DJ<div data-tab-content></div></li>
+				</ul>
+				<p>$1,600</p>
+			</div>
+		</div>
 	</div>
 </section>
