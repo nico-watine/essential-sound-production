8722d48399fee248f5bec309e62553feba3f39a7
Nico Watine <nico@studioncreations.com>
2016-04-28 12:00:05 -0500

    Got down structure of <li> labels without tab-content
    So now the container has function accordion feature (selectively)
    Now will adjust display:block on the <a>

---
+++ src/pages/services.php
@@ -0,0 +1,43 @@
+---
+body-id: services
+---
+<section class="services">
+	<div class="row text-center" id="row-title">
+		<h2 class="title">OUR SERVICES</h2>
+		<h3 class="body-font expanded">We offer a variety of event service packages to best fit your needs.</h3>
+	</div>
+	<div class="row text-center">
+		<div class="small-12 columns">
+			<div class="package-plan" id="premium">
+				<h3 class="title">PREMIUM</h3>
+				<ul class="accordion" data-accordion data-allow-all-closed="true">
+					<li>6 Hours of Music<div data-tab-content></div></li>
+					<li>Premium QSC Sound Speakers<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Speaker Details</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>2 Aluminum Truss &amp; 2 Moving Light Heads<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>10 Up Lighting<div data-tab-content></div></li>
+					<li class="accordion-item" data-accordion-item>
+						<a href="#" class="accordion-title">Image Examples</a>
+						<div class="accordion-content" data-tab-content>
+							2 - K12 Speakers <br>1 - KSub Speaker
+						</div>
+					</li>
+					<li>2 LED Dance Lights<div data-tab-content></div></li>
+					<li>Wireless Mic &amp; DJ<div data-tab-content></div></li>
+				</ul>
+				<p>$1,000</p>
+			</div>
+		</div>
+	</div>
+</section>
